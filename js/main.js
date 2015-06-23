f0939012735de980ef8a05e2c2b8b99ca170f63c
Nigel Anderson <nigel.m.anderson@gmail.com>
2015-06-23 11:28:54 +0200

    switch to lazy loading of slides

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@ $(function() { // WRAP EVERYTHING UP IN DOM READY
     $sV.slick({
       autoplay: true,
       autoplaySpeed: 3000,
+      lazyLoad: 'ondemand',
       centerMode: true,
       variableWidth: true,
       speed: 600,
@@ -54,6 +55,7 @@ $(function() { // WRAP EVERYTHING UP IN DOM READY
     $sF.slick({
       infinite: true,
       speed: 600,
+      lazyLoad: 'ondemand',
       slidesToShow: 1,
       centerMode: true,
       variableWidth: true,
